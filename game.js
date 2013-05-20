2ef07f89ce2abdc04c87bcf2d4135fb149d391ad
Elaine Matthias <elaine.matthias@gmail.com>
2013-05-20 19:50:01 -0400

    place armies on table
    also added jquery

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
 // JS code for game goes here
 $(function(){
-  $("#gameboard").text("Hello world!");
+
+placeArmies(0,3,2);
+placeArmies(3,0,2);
+	
 });
+
+function placeArmies (row, col, num){
+	var tr = $($("#gameboard tr")[row]);
+	var td = $(tr.find("td")[col]);
+	td.text("" + num);
+	}
