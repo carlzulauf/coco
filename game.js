9b238445f46a7ba31fcd6cd6ceaa37b663bf38c6
Carl Zulauf <carl@linkleaf.com>
2013-05-22 11:17:04 -0400

    Some more refactoring

--- game.js
+++ game.js
@@ -3,41 +3,49 @@ $(function(){
 });
 
 // generates and returns a game object, which keeps track of the game state
-function Game(width, height) {
-  this.players = [
-    new Player(0),
-    new Player(1)
-  ];
-  this.currentPlayerNumber = 0;
-  this.territories = {};
-  this.board = new GameBoard( $("#gameboard") );
+var Game = (function(){
+  function Game(width, height) {
+    this.players = [
+      new Player(0),
+      new Player(1)
+    ];
+    this.currentPlayerNumber = 0;
+    this.territories = {};
+    this.selectedTerritory = null;
+    this.board = new GameBoard( this, $("#gameboard") );
 
-  this.board.setPlayer( this.currentPlayer() );
+    this.board.setPlayer( this.currentPlayer() );
 
-  // loop through all the rows and columns
-  for(var row = 0; row < height; row++) {
-    for(var col = 0; col < width; col++) {
-      // create a new territory for each cell on the game board
-      var id = row + "," + col,
-          territory = new Territory(row, col);
-      this.territories[id] = territory;
-      this.board.drawTerritory(territory);
+    // loop through all the rows and columns
+    for(var row = 0; row < height; row++) {
+      for(var col = 0; col < width; col++) {
+        // create a new territory for each cell on the game board
+        var id = row + "," + col,
+            territory = new Territory(this, row, col);
+        this.territories[id] = territory;
+        this.board.drawTerritory(territory);
+      }
     }
+
+    // place initial armies in top right and bottom left
+    this.placeArmies("0,3", 2, this.players[0]);
+    this.placeArmies("3,0", 2, this.players[1]);
   }
 
-  // place initial armies in top right and bottom left
-  this.placeArmies("0,3", 2, this.players[0]);
-  this.placeArmies("3,0", 2, this.players[1]);
-}
-Game.prototype.placeArmies = function(territory_id, armies, player) {
-  var territory = this.territories[territory_id];
-  territory.armies = armies;
-  territory.owner = player;
-  this.board.drawTerritory(territory);
-}
-Game.prototype.currentPlayer = function() {
-  return this.players[ this.currentPlayerNumber ];
-}
+  var p = Game.prototype;
+
+  p.placeArmies = function(territory_id, armies, player) {
+    var territory = this.territories[territory_id];
+    territory.armies = armies;
+    territory.owner = player;
+    this.board.drawTerritory(territory);
+  }
+  p.currentPlayer = function() {
+    return this.players[ this.currentPlayerNumber ];
+  }
+
+  return Game;
+}());
 
 var Player = (function(){
   var colors = ["green", "blue", "red", "orange"];
@@ -52,8 +60,9 @@ var Player = (function(){
   return Player;
 }());
 
-function Territory(row, col) {
+function Territory(game, row, col) {
   // set up territory, neighbors, etc
+  this.game = game;
   this.armies = 0;
   this.row = row;
   this.col = col;
@@ -62,19 +71,18 @@ function Territory(row, col) {
 
 var GameBoard = (function(){
   // constructor
-  function board(content) {
+  function GameBoard(game, content) {
+    this.game = game;
     this.content = content;
   }
-  var p = board.prototype;
+  var p = GameBoard.prototype;
 
   // public functions
   p.findCell = function(territory) {
     if (!territory.content) {
       var tr = $( this.content.find("tr")[territory.row] ),
           td = $(           tr.find("td")[territory.col] );
-      td.click(function(e){
-        td.css("background-color", "red");
-      });
+      td.click(function(e){ this.game.selectTerritory(territory) });
       territory.content = td;
     }
     return territory.content;
@@ -92,5 +100,5 @@ var GameBoard = (function(){
     this.content.find(".current-player").text(player.name);
   }
 
-  return board;
+  return GameBoard;
 }());
