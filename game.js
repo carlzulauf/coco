f1c719485f2d797fe6ab01b0dbf10f3b4942960f
Carl Zulauf <carl@linkleaf.com>
2013-05-22 14:31:06 -0400

    Move starting armies and positions back to their original

--- game.js
+++ game.js
@@ -29,8 +29,8 @@ var Game = (function(){
     }
 
     // place initial armies in top right and bottom left
-    this.placeArmies("1,1", 4, this.players[0]);
-    this.placeArmies("1,2", 2, this.players[1]);
+    this.placeArmies("0,3", 2, this.players[0]);
+    this.placeArmies("3,0", 2, this.players[1]);
   }
 
   var p = Game.prototype;
