af9ad4f942b5cbd7d0c5ddba42237b5ea9b3a6d0
Carl Zulauf <carl@linkleaf.com>
2013-05-21 22:53:06 -0400

    Add objects to keep track of game, players, territories, and the game board.

--- game.js
+++ game.js
@@ -1,10 +1,61 @@
 $(function(){
-  placeArmies(0,3,2);
-  placeArmies(3,0,2);
+  window.currentGame = new Game(4, 4);
 });
 
-function placeArmies (row, col, num){
-  var tr = $($("#gameboard tr")[row]);
-  var td = $(tr.find("td")[col]);
-  td.text("" + num);
+// generates and returns a game object, which keeps track of the game state
+var Game = function(width, height) {
+  this.players = [
+    new Player(0),
+    new Player(1)
+  ];
+  this.currentPlayerNumber = 0;
+  this.territories = {};
+  this.board = new GameBoard( $("#gameboard") );
+
+  // loop through all the rows and columns
+  for(var row = 0; row < height; row++) {
+    for(var col = 0; col < width; col++) {
+      // create a new territory for each cell on the game board
+      var id = row + "," + col,
+          territory = new Territory(row, col);
+      this.territories[id] = territory;
+      this.board.drawTerritory(territory);
+    }
+  }
+
+  // place initial armies in top right and bottom left
+  this.placeArmies("0,3", 2);
+  this.placeArmies("3,0", 2);
+};
+
+Game.prototype.placeArmies = function(territory_id, armies) {
+  var territory = this.territories[territory_id];
+  territory.armies = armies;
+  this.board.drawTerritory(territory);
+}
+
+function Player(num) {
+  this.id = num;
+  this.num = num;
+}
+
+function Territory(row, col) {
+  // set up territory, neighbors, etc
+  this.armies = 0;
+  this.row = row;
+  this.col = col;
+  this.owner = null;
+}
+
+function GameBoard(content) {
+  // setup game board
+  this.content = content;
+}
+GameBoard.prototype.drawTerritory = function(territory) {
+  var tr = $( this.content.find("tr")[territory.row] ),
+      td = $(           tr.find("td")[territory.col] ),
+      text;
+  if (territory.armies > 0) text = territory.armies + "";
+  else text = "[]";
+  td.text(text);
 }
